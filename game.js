ae62bd87f592ef20f8453aa00ecc23bb3932a493
Mads Johansen Koch <mkoch@vertic.com>
2014-10-17 14:56:58 -0400

    bugfix

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-// Create the canvas
+
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 1500;
 canvas.height = 1000;
 document.body.appendChild(canvas);
 
-// Background image
+
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
@@ -13,7 +13,7 @@ bgImage.onload = function () {
 };
 bgImage.src = "images/background1.png";
 
-// Hero image
+
 var heroReady = false;
 var heroImage = new Image();
 heroImage.onload = function () {
@@ -21,7 +21,7 @@ heroImage.onload = function () {
 };
 heroImage.src = "images/hero.png";
 
-// Monster image
+
 var monsterReady = false;
 var monsterImage = new Image();
 monsterImage.onload = function () {
@@ -29,14 +29,14 @@ monsterImage.onload = function () {
 };
 monsterImage.src = "images/monster.png";
 
-// Game objects
+
 var hero = {
-	speed: 500 // movement in pixels per second
+	speed: 500
 };
 var monster = {};
 var monstersCaught = 0;
 
-// Handle keyboard controls
+
 var keysDown = {};
 
 addEventListener("keydown", function (e) {
@@ -47,32 +47,32 @@ addEventListener("keyup", function (e) {
 	delete keysDown[e.keyCode];
 }, false);
 
-// Reset the game when the player catches a monster
+
 var reset = function () {
 	hero.x = canvas.width / 2;
 	hero.y = canvas.height / 2;
 
-	// Throw the monster somewhere on the screen randomly
+
 	monster.x = 32 + (Math.random() * (canvas.width - 64));
 	monster.y = 32 + (Math.random() * (canvas.height - 64));
 };
 
-// Update game objects
+
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
+	if (38 in keysDown) { 
 		hero.y -= hero.speed * modifier;
 	}
-	if (40 in keysDown) { // Player holding down
+	if (40 in keysDown) { 
 		hero.y += hero.speed * modifier;
 	}
-	if (37 in keysDown) { // Player holding left
+	if (37 in keysDown) { 
 		hero.x -= hero.speed * modifier;
 	}
-	if (39 in keysDown) { // Player holding right
+	if (39 in keysDown) { 
 		hero.x += hero.speed * modifier;
 	}
 
-	// Are they touching?
+	
 	if (
 		hero.x <= (monster.x + 32)
 		&& monster.x <= (hero.x + 32)
@@ -84,7 +84,7 @@ var update = function (modifier) {
 	}
 };
 
-// Draw everything
+
 var render = function () {
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
@@ -98,7 +98,7 @@ var render = function () {
 		ctx.drawImage(monsterImage, monster.x, monster.y);
 	}
 
-	// Score
+
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "12px Helvetica";
 	ctx.textAlign = "left";
@@ -106,7 +106,7 @@ var render = function () {
 	ctx.fillText("Cakes slaughtered: " + monstersCaught, 32, 32);
 };
 
-// The main game loop
+
 var main = function () {
 	var now = Date.now();
 	var delta = now - then;
@@ -116,15 +116,15 @@ var main = function () {
 
 	then = now;
 
-	// Request to do this again ASAP
+
 	requestAnimationFrame(main);
 };
 
-// Cross-browser support for requestAnimationFrame
+
 var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
-// Let's play this game!
+
 var then = Date.now();
 reset();
 main();
